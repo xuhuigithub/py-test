b810d03b1125936b935273fdd7ee5e0209cb720d
xuhuigithub <1498472791@qq.com>
2017-08-13 21:15:21 +0800

    修改了注释

--- foundation/day5/thread_demo.py
+++ foundation/day5/thread_demo.py
@@ -1,7 +1,7 @@
 #!/bin/env python
 #coding:utf-8
 #当运行这个程序时创建了一个主进程带有一个主线程，主线程又创造了一个子线程去执行函数Foo
-#子线程的最大存活时间不超过主线程的最大存活时间，也就是说当主线程销毁时子线程也就没有了
+#子线程的最大存活时间不超过主线程的最大存活时间，也就是说当主线程销毁时子线程也就没有了，不管子线程是否设置在后台运行
 
 from threading import Thread
 import time
@@ -13,15 +13,21 @@ def Foo(arg):
 
 
 print 'before'
-t1 = Thread(target=Foo,kwargs={'arg':'xuhui',},name='hehe')
+t1 = Thread(target=Foo,kwargs={'arg':'xuhui',})
 print t1.isDaemon()
-t1.setDaemon(True)
+#t1.setDaemon(True)
 #将子线程发到后台去执行，变为守护线程
 t1.start()
 #t1.join()
 #join方法会阻塞主进程，直到子进程处理完毕，处理子进程时可以设置超时时间，默认为None无限制
 print t1.getName()
-print 'after'
-
 time.sleep(10)
+print 'after'
+print '还会阻塞吗'
+#time.sleep(10)
 #将主进程阻塞住10秒保证其不退出，子进程就不会结束
+
+#time.sleep 会阻塞住主进程
+#time.start 也会阻塞主进程
+#子进程可以在主进程阻塞时执行
+#进程的join方法也会阻塞主进程
