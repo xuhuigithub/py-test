b3306fd9c3667ae3d49e87575375a6cdf02c14da
xuhui <1498472791@qq.com>
2017-08-22 19:35:12 +0800

    增加多进程之间使用Queue共享内存

--- foundation/day7/mutliprocess_share_memory_queue.py
+++ foundation/day7/mutliprocess_share_memory_queue.py
@@ -1,14 +1,19 @@
 #!/usr/bin/env python
 #encoding: utf-8
 from multiprocessing import Process,Queue
-from Queue import Queue as Queue2
+# import Queue as Q2
 
-def f(q,n):
-  q.put([n,'hello'])
+def f(q):
+  #为什么不能global，queue呢？
+  #每个进程之间不共享内存，你global一次也就是复制一次主进程的queue
+  q.put([42,None,'hello'])
 
 if __name__ == "__main__":
   q = Queue()
-  for i in range(5):
-    p = Process(target=f,args=(q,i,))
-    p.start()
-    print q.get()
+  #这个queue是主进程的queue
+  p = Process(target=f,args=(q,))
+  #1、子进程向queue中放入数据
+  p.start()
+  p.join()
+  print q.get()
+  #2、父进程拿到数据
