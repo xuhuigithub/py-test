5ff2662898e88173f6078bc15fe8cdb664a60c30
xuhui <1498472791@qq.com>
2017-09-05 22:09:59 +0800

    修改了笔记，增加了HMAC的使用方法

--- foundation/day3/md5demo.py
+++ foundation/day3/md5demo.py
@@ -1,15 +1,23 @@
 #!/bin/env python
 #coding:utf-8
 import hashlib
+import hmac
 hashmd5 = hashlib.md5()
 hashmd5.update('admin')
+#update方法随时可以向加字符串到里边去
 print hashmd5.digest()
-#以十六进制显示
+#以二进制显示
 print hashmd5.hexdigest()
-
+#以十六进制显示
 temp = hashlib.sha256()
 temp.update('admin')
 print temp.digest(),temp.hexdigest()
 x = '8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918'
 if x == temp.hexdigest():
   print 'hehe'
+
+secret = hmac.new('secret_key')
+secret.update('test message')
+message = secret.hexdigest()
+print message
+#在原有的基础上再加密一层（使用自己生成的Key）
